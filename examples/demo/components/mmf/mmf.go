f6697b8463a2656b7385b8647a80fc47f99db1b9
Scott Redig <sredig@google.com>
2019-10-14 17:13:13 -0700

    progress

---
+++ examples/demo/components/mmf/mmf.go
@@ -0,0 +1,166 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package mmf
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"open-match.dev/open-match/examples/demo/components"
+	mmfHarness "open-match.dev/open-match/pkg/harness/function/golang"
+	"open-match.dev/open-match/pkg/pb"
+)
+
+func Run(ds *components.DemoShared) {
+	mmfHarness.RunMatchFunction(&mmfHarness.FunctionSettings{
+		Func: MakeMatches,
+	})
+
+}
+
+func MakeMatches(p *mmfHarness.MatchFunctionParams) ([]*pb.Match, error) {
+	tickets, ok := p.PoolNameToTickets["Everyone"]
+	if !ok {
+		return nil, errors.New("Expected pool named Everyone.")
+	}
+
+	var matches []*pb.Match
+	t := time.Now().Format("2006-01-02T15:04:05.00")
+
+	for i := 0; i+1 < len(tickets); i += 2 {
+		matches = append(matches, &pb.Match{
+			MatchId:       fmt.Sprintf("profile-%s-time-%s-num-%d", p.ProfileName, t, i/2),
+			MatchProfile:  p.ProfileName,
+			MatchFunction: "first-match-mmf",
+			Tickets: []*pb.Ticket{
+				tickets[i], tickets[i+1],
+			},
+		})
+	}
+
+	return matches, nil
+}
+
+// u := updater.NewNested(ds.Ctx, ds.Update)
+
+// for i := 0; i < 5; i++ {
+//   name := fmt.Sprintf("fakeplayer_%d", i)
+//   go func() {
+//     for !isContextDone(ds.Ctx) {
+//       runScenario(ds.Ctx, name, u.ForField(name))
+//     }
+//   }()
+// }
+
+// func isContextDone(ctx context.Context) bool {
+// select {
+// case <-ctx.Done():
+//   return true
+// default:
+//   return false
+// }
+// }
+
+// type status struct {
+//   Status     string
+//   Assignment *pb.Assignment
+// }
+
+// func runScenario(ctx context.Context, name string, update updater.SetFunc) {
+//   defer func() {
+//     r := recover()
+//     if r != nil {
+//       err, ok := r.(error)
+//       if !ok {
+//         err = fmt.Errorf("pkg: %v", r)
+//       }
+
+//       update(status{Status: fmt.Sprintf("Encountered error: %s", err.Error())})
+//       time.Sleep(time.Second * 10)
+//     }
+//   }()
+
+//   s := status{}
+
+//   //////////////////////////////////////////////////////////////////////////////
+//   s.Status = "Main Menu"
+//   update(s)
+
+//   time.Sleep(time.Duration(rand.Int63()) % (time.Second * 15))
+
+//   //////////////////////////////////////////////////////////////////////////////
+//   s.Status = "Connecting to Open Match frontend"
+//   update(s)
+
+//   // See https://open-match.dev/site/docs/guides/api/
+//   conn, err := grpc.Dial("om-frontend.open-match.svc.cluster.local:50504", grpc.WithInsecure())
+//   if err != nil {
+//     panic(err)
+//   }
+//   defer conn.Close()
+//   fe := pb.NewFrontendClient(conn)
+
+//   //////////////////////////////////////////////////////////////////////////////
+//   s.Status = "Creating Open Match Ticket"
+//   update(s)
+
+//   var ticketId string
+//   {
+//     req := &pb.CreateTicketRequest{
+//       Ticket: &pb.Ticket{},
+//     }
+
+//     resp, err := fe.CreateTicket(ctx, req)
+//     if err != nil {
+//       panic(err)
+//     }
+//     ticketId = resp.Ticket.Id
+//   }
+
+//   //////////////////////////////////////////////////////////////////////////////
+//   s.Status = fmt.Sprintf("Waiting match with ticket Id %s", ticketId)
+//   update(s)
+
+//   var assignment *pb.Assignment
+//   {
+//     req := &pb.GetAssignmentsRequest{
+//       TicketId: ticketId,
+//     }
+
+//     stream, err := fe.GetAssignments(ctx, req)
+//     for assignment.GetConnection() == "" {
+//       resp, err := stream.Recv()
+//       if err != nil {
+//         // For now we don't expect to get EOF, so that's still an error worthy of panic.
+//         panic(err)
+//       }
+
+//       assignment = resp.Assignment
+//     }
+
+//     err = stream.CloseSend()
+//     if err != nil {
+//       panic(err)
+//     }
+//   }
+
+//   //////////////////////////////////////////////////////////////////////////////
+//   s.Status = "Sleeping (pretend this is playing a match...)"
+//   s.Assignment = assignment
+//   update(s)
+
+//   time.Sleep(time.Second * 10)
+// }
