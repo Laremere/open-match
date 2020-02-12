cc26d560c6c328af3f7f7bacc521583686d46d43
Scott Redig <laremere@gmail.com>
2020-02-11 21:00:12 -0800

    add large team shooter scenario

--- examples/scale/scenarios/scenarios.go
+++ examples/scale/scenarios/scenarios.go
@@ -60,6 +60,7 @@ var activeScenario = func() *Scenario {
 	// so it's easier to run different scenarios without changing code.
 	gs = battleroyal.Scenario()
 	gs = teamshooter.Scenario()
+	gs = teamshooter.ScenarioLarge()
 
 	return &Scenario{
 		BackendAssignsTickets: true,
